5c72479ac15fccab33b94a4ada8d29b4a38ad2a1
rafiraa36 <rafiadliazmi@gmail.com>
2026-09-03 13:46:42 +0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AEJApp from "./AEJApp";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AEJApp />
+  </React.StrictMode>
+);
